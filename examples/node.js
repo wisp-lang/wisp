6f4db678ad2439faf41dcb3a0ac225b2d78992a4
Irakli Gozalishvili <rfobic@gmail.com>
2013-12-15 16:28:41 -0800

    Delete some examples.

--- examples/node.js
+++
@@ -1,8 +0,0 @@
-{
-    var _ns_ = {
-            id: 'wisp.examples.node',
-            doc: void 0
-        };
-}
-console.log('hello node');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9leGFtcGxlcy9ub2RlLmpzIiwic291cmNlcyI6WyIuL2V4YW1wbGVzL25vZGUud2lzcCJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQSxDQUFBO0FBQUEsSTs7O1VBQUE7QUFBQSxDO0FBRUFBLE9BQUEsQ0FBUUMsR0FBUixDLFlBQUEsQyJ9
--- examples/node.wisp
+++
@@ -1,3 +0,0 @@
-(ns wisp.examples.node)
-
-(print "hello node")
